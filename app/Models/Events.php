ada80e7866f43ebbc0998561e7a9ca2289a96231
Vicky <vicky@tickleright.in>
2023-09-30 11:03:46 +0530

    events veiw added

--- app/Models/Events.php
+++ app/Models/Events.php
@@ -10,4 +10,34 @@ class Events extends Model
     use HasFactory;
     protected $table = "events";
     protected $primaryKey = "id";
+
+    // public function advancement()
+    // {
+    //     return $this->belongsTo(Advancement::class);
+    // }
+
+    // public function eventType()
+    // {
+    //     return $this->belongsTo(EventType::class);
+    // }
+
+    // public function flightOrder()
+    // {
+    //     return $this->belongsTo(FlightOrder::class);
+    // }
+
+    // public function meet()
+    // {
+    //     return $this->belongsTo(Meet::class, 'meet_id');
+    // }
+
+    // public function positionAssignment()
+    // {
+    //     return $this->belongsTo(PositionAssignment::class);
+    // }
+
+    // public function flightAssignment()
+    // {
+    //     return $this->belongsTo(FlightAssignment::class);
+    // }
 }
