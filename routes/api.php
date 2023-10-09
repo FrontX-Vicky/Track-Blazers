48ee9b91db7e342a083361e2efbe726919232395
Vicky <vicky@tickleright.in>
2023-10-09 15:09:51 +0530

    api data added

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Meets;
 use App\Models\Events;
+use App\Models\Scoresheet;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,12 +24,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/authenticat_meet', function(Request $req){
   $request = $req->all();
-  // $meet = Meets::where('meet_login_id', '=',  $request['meet_login_id'])->where('password', '=',  $request['password'])->get();
-  $meet = Meets::where('meet_login_id', '=',  $request['meet_login_id'])->get();
-  $meet = $meet->toArray()[0];
-  $events = Events::where('meet_id', '=', $meet['id'])->get();
-  $data['meet'] = $meet;
-  $data['events'] = $events;
+  $data = [];
+  if(isset($request['meet_login_id'])){
+    if(isset($request['password'])){
+      $meet = Meets::where('meet_login_id', '=',  $request['meet_login_id'])->where('password', '=',  $request['password'])->get();
+      // $meet = Meets::where('meet_login_id', '=',  $request['meet_login_id'])->get();
+      $meet = $meet->toArray()[0];
+      $events = Events::where('meet_id', '=', $meet['id'])->get();
+      $athletes = Scoresheet::where('meet_id', '=',  $meet['id'])->get();
+      $data['meet'] = $meet;
+      $data['events'] = $events;
+      $data['athletes'] = $athletes;
+      $data['error'] = 0;
+      $data['msg'] = "success";
+    }else{
+      $data['error'] = 1;
+      $data['msg'] = "password not present in request";
+    }
+  }else{
+    $data['error'] = 1;
+    $data['msg'] = "meet_login_id not present in request";
+  }
+  // dd($request);
   return json_encode($data);
 });
 
