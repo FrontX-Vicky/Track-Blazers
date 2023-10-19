ed138bb057f326b4e2707f42e19c6feedb4aef69
Vicky <vicky@tickleright.in>
2023-10-19 12:27:35 +0530

    file delete

---
+++ app/Models/Scoresheet_v2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Scoresheet_v2 extends Model
+{
+    use HasFactory;
+    protected $table = "score_sheet_v2";
+    protected $primaryKey = "id";
+}
