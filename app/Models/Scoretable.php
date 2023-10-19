aadcaca8ff31236a2faff6acd87d051e3b1caba6
Vicky <vicky@tickleright.in>
2023-10-19 12:19:22 +0530

    table change

--- app/Models/Scoretable.php
+++ app/Models/Scoretable.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Scoretable extends Model
 {
     use HasFactory;
-    protected $table = "score_table";
+    protected $table = "score_table_v1";
     protected $primaryKey = "id";
 }
